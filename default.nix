99503810f26d12e8223f1dc9eecb3afe14c0fec6
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-04-12 16:08:49 -0700

    refactor: remove unecessary exports from nix config

--- default.nix
+++ default.nix
@@ -27,20 +27,17 @@ pkgs.stdenv.mkDerivation {
   src = ./.;
 
   configurePhase = ''
-    export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib"
     mkdir -p $TMPDIR/build
     cd $TMPDIR/build
     cmake $src
   '';
 
   buildPhase = ''
-    export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib"
     cd $TMPDIR/build
     make
   '';
 
   installPhase = ''
-    export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib"
     mkdir -p $out/bin
     cp $TMPDIR/build/fmarch $out/bin/fmarch
     wrapProgram $out/bin/fmarch \
