30c8d68bb2de261b2e78ca9cd4e923d018aad808
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-04-11 17:43:15 -0700

    docs: add comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include "window_callbacks.h"
 #include "constants.h"
 
+/* initialize GLAD and GLFW, then create window object */
 void createGLcontexts(GLFWwindow **window) {
   glfwInit();
 
@@ -13,6 +15,7 @@ void createGLcontexts(GLFWwindow **window) {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   #ifdef __APPLE__
+    /* set compatability mode for macOS */
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   #endif
 
@@ -34,11 +37,13 @@ void createGLcontexts(GLFWwindow **window) {
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
+/* called in main render loop */
 void render(GLFWwindow *window) {
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_ACCUM_BUFFER_BIT);
 }
 
+/* clean up objects and exit */
 void cleanup(GLFWwindow *window) {
   glfwDestroyWindow(window);
   glfwTerminate();
@@ -50,6 +55,7 @@ int main() {
   createGLcontexts(&window);
   window_callbacks::setGLFWcallbacks(window);
 
+  /* main render loop */
   while(!glfwWindowShouldClose(window)) {
     render(window);
     glfwSwapBuffers(window);
