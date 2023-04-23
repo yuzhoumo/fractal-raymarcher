2da592c9bbfdf4b087164df350dd62ef41c29d5d
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-04-23 13:05:56 -0700

    feat: add support for raymarched fragment shaders
    Render a fullscreen quad (two triangles) and set necessary uniforms for
    the fragment shader. Add a keyboard shortcut to reload shaders. Also
    remove old vertex and fragment shader files.

--- src/include/constants.hpp
+++ src/include/constants.hpp
@@ -23,4 +23,19 @@ const glm::vec3 CAMERA_DEFAULT_POSITION { 0.0f,  0.0f,  0.0f };
 const glm::vec3 CAMERA_DEFAULT_WORLD_UP { 0.0f,  1.0f,  0.0f };
 const glm::vec3 CAMERA_DEFAULT_FRONT    { 0.0f,  0.0f, -1.0f };
 
+/* fullscreen quad (two triangles) for the vertex shader since raymarching is
+ * done entirely in fragent shader */
+const float FS_QUAD[] = {
+  -1.0f, -1.0f, 0.0f,
+   1.0f, -1.0f, 0.0f,
+  -1.0f,  1.0f, 0.0f,
+   1.0f,  1.0f, 0.0f
+};
+
+/* define opengl function params for fullscreen quad */
+#define FS_QUAD_VERTEX_ATTRIB_INDEX 0
+#define FS_QUAD_DRAW_ARRAY_PARAMS GL_TRIANGLE_STRIP, 0, 4
+#define FS_QUAD_VERTEX_ATTRIB_PARAMS FS_QUAD_VERTEX_ATTRIB_INDEX, \
+    3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0
+
 #endif /* ifndef CONSTANTS_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
-#include <cstdlib>
 #include <iostream>
 #include <filesystem>
-#include <math.h>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -65,6 +63,7 @@ int main(const int argc, const char* argv[]) {
     std::cerr << "ERROR::INCORRECT_ARG_COUNT: " << argc << ", expected 3."
       << std::endl;
     exit(EXIT_FAILURE);
+
   }
 
   /* populate display singleton before glfw window creation */
@@ -84,70 +83,14 @@ int main(const int argc, const char* argv[]) {
   if (!frag_shader_path.is_absolute())
     frag_shader_path = std::filesystem::absolute(frag_shader_path);
 
-  const Shader shader = Shader{vert_shader_path.string(),
-                               frag_shader_path.string()};
+  Shader shader = Shader{vert_shader_path.string(),
+                         frag_shader_path.string()};
   if (0 == shader.getId()) {
     std::cerr << "ERROR::SHADER_CREATION_FAILURE" << std::endl;
     cleanup(window, EXIT_FAILURE);
   }
 
-  // <<< TEST CODE: set up cubs in vertex buffer
-  float vertices[] = {
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-  };
-
-  glm::vec3 cubePositions[] = {
-    glm::vec3( 0.0f,  0.0f,  0.0f),
-    glm::vec3( 2.0f,  5.0f, -15.0f),
-    glm::vec3(-1.5f, -2.2f, -2.5f),
-    glm::vec3(-3.8f, -2.0f, -12.3f),
-    glm::vec3( 2.4f, -0.4f, -3.5f),
-    glm::vec3(-1.7f,  3.0f, -7.5f),
-    glm::vec3( 1.3f, -2.0f, -2.5f),
-    glm::vec3( 1.5f,  2.0f, -2.5f),
-    glm::vec3( 1.5f,  0.2f, -1.5f),
-    glm::vec3(-1.3f,  1.0f, -1.5f)
-  };
+  /* load fullscreen quad */
 
   GLuint VAO, VBO;
   glGenVertexArrays(1, &VAO);
@@ -155,47 +98,41 @@ int main(const int argc, const char* argv[]) {
 
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
-  // <<< TEST CODE
+  glBufferData(GL_ARRAY_BUFFER, sizeof(FS_QUAD), FS_QUAD, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(FS_QUAD_VERTEX_ATTRIB_PARAMS);
+  glEnableVertexAttribArray(FS_QUAD_VERTEX_ATTRIB_INDEX);
 
   /* main render loop */
   while(!glfwWindowShouldClose(window)) {
     Display::updateTime(glfwGetTime());
     Display::updateCamera(window);
 
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+      shader = Shader{vert_shader_path.string(),
+                      frag_shader_path.string()};
+
     /* clear frame buffer */
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /* activate shader and set matrices */
+    /* activate shader and set uniforms */
     shader.use();
-    shader.setMat4("projection", display.projection_matrix);
-    shader.setMat4("view", display.camera.getViewMatrix());
-
-    // >>> TEST CODE 
+    shader.setBool("u_supersample", display.supersample);
+    shader.setFloat3("u_camera_pos", display.camera.getPosition());
+    shader.setFloat3("u_camera_front", display.camera.getFront());
+    shader.setFloat3("u_camera_up", display.camera.getUp());
+    shader.setFloat3("u_camera_right", display.camera.getRight());
+    shader.setFloat("u_fov", display.camera.getFOV());
+    shader.setFloat("u_time", display.time);
+    shader.setFloat2("u_resolution", glm::vec2(display.window_width,
+                                               display.window_height));
+    /* draw frame */
     glBindVertexArray(VAO);
-    for (unsigned int i = 0; i < 10; i++) {
-      // calculate model matrix for each
-      glm::mat4 model = glm::mat4(1.0f); // identity matrix
-      model = glm::translate(model, cubePositions[i]);
-      float angle = 20.0f * i;
-      model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-      shader.setMat4("model", model);
-
-      glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
-
-    float green_value = (sin(display.time) / 2.0f) + 0.5f;
-    shader.setFloat4("ourColor", glm::vec4(0.0f, green_value, 0.0f, 1.0f));
-    shader.setFloat("xoffset", cos(display.time) / 2.0);
-    shader.setFloat("yoffset", sin(display.time) / 2.0);
-
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    // <<< TEST CODE
-
+    glDrawArrays(FS_QUAD_DRAW_ARRAY_PARAMS);
     glfwSwapBuffers(window);
+
+    /* poll for window events */
     glfwPollEvents();
   }
 
